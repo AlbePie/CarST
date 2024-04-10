afceea7c47e8947e56f330f5aa2e53a70645f95d
AlbePie <109433291+AlbePie@users.noreply.github.com>
2024-04-10 17:35:01 +0200

    Recreated the UI
    Added localisation table, completely recreated the UI.

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -6,6 +6,7 @@ extends Node3D
 
 @export_group("Node References")
 @export var car:VehicleBody3D
+@export var publicpopup:PopupPanel
 @export var colorpicker:ColorPicker
 @export var coloroptions:OptionButton
 
@@ -14,17 +15,22 @@ var uses_custom_color = false
 var colors = [Color("770f0f"), Color("0f0f77"), Color("0f550f"), Color("bbbb1f")]
 
 func _ready():
+	get_window().min_size = Vector2i(1280, 768)
+	TranslationServer.set_locale("en_US")
+	
+	
 	if start_immediately:
 		_on_play_button_pressed()
+	
 	management.color_updated.connect(car.update_color)
-	_on_color_selected($Menu/ColorPick/Options.selected)
+	_on_color_selected(coloroptions.selected)
 
 func _process(delta):
 	car.rotation_degrees.y += delta * (360 / turn)
 
 
 func _on_color_selected(index):
-	if coloroptions.get_item_text(index) == "Custom":
+	if coloroptions.get_item_text(index) == "{color.custom}":
 		uses_custom_color = true
 		management.set_color(colorpicker.color)
 	else:
@@ -40,3 +46,7 @@ func _on_play_button_pressed():
 
 func _on_color_picker_color_changed(color):
 	management.set_color(color)
+
+
+func _on_play_public_pressed():
+	publicpopup.popup_centered(get_window().size - Vector2i.ONE * 50)
